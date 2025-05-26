6f434e8f31e40720d3cf15651452aa58551cd8df
ireshdilsh <ireshdilshan46@gmail.com>
2025-05-26 19:56:42 +0530

    Refactor MainScreen layout for improved readability and consistency

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -7,7 +7,7 @@ export default function MainScreen() {
   const videoRef = useRef(null);
 
   return (
-    <View style={{height:'100%',width:'100%'}}>
+    <View style={{ height: '100%', width: '100%' }}>
       <Video
         ref={videoRef}
         source={require('../assets/hero-video.mp4')}
@@ -17,13 +17,13 @@ export default function MainScreen() {
         isLooping
         shouldPlay
       />
-      <LinearGradient colors = {['#fff','transparent']} style={{ height: '100%', width: '100%',position:'absolute',padding:40}}>
-        <Text style={{fontSize:16,marginTop:390,fontWeight:700,color:'#fff'}}>Your Journey, Your Way_</Text>
-        <Text style={{color:'#fff',fontSize:36,fontWeight:800}}>Explore the World</Text>
-        <Text style={{color:'#fff',fontSize:36,fontWeight:800}}>with Confidence</Text>
-        <Text style={{color:'#fff',textAlign:'justify',marginTop:8}}>From sun-soaked beaches to mountain escapes, we offer personalized travel experiences and unforgettable memories.</Text>
-        <TouchableOpacity style={{marginTop:55,justifyContent:'center',alignItems:'center',backgroundColor:'#0d6efd',height:48,borderRadius:5}}>
-          <Text style={{color:'#fff',fontSize:14,fontWeight:600}}>Get Stated</Text>
+      <LinearGradient colors={['#fff', 'transparent']} style={{ height: '100%', width: '100%', position: 'absolute', padding: 40 }}>
+        <Text style={{ fontSize: 16, marginTop: 390, fontWeight: 700, color: '#fff' }}>Your Journey, Your Way_</Text>
+        <Text style={{ color: '#fff', fontSize: 36, fontWeight: 800 }}>Explore the World</Text>
+        <Text style={{ color: '#fff', fontSize: 36, fontWeight: 800 }}>with Confidence</Text>
+        <Text style={{ color: '#fff', textAlign: 'justify', marginTop: 8 }}>From sun-soaked beaches to mountain escapes, we offer personalized travel experiences and unforgettable memories.</Text>
+        <TouchableOpacity style={{ marginTop: 55, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0d6efd', height: 48, borderRadius: 5 }}>
+          <Text style={{ color: '#fff', fontSize: 14, fontWeight: 600 }}>Get Stated</Text>
         </TouchableOpacity>
       </LinearGradient>
     </View>
