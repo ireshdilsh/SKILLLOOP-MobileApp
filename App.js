2ba14edb05e5d7f0080ba868326a7651288382a2
ireshdilsh <ireshdilshan46@gmail.com>
2025-05-28 09:15:05 +0530

    Update Stack.Navigator to include screenOptions for consistent header visibility

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default function App() {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Landing'>
-        <Stack.Screen name="Landing" component={MainScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Navigator initialRouteName='Landing' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Landing' component={MainScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
